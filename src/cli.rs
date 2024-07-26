f9c4369d92e9edd7351759dcb58b72ff4db3a0dc
Lars Sjöberg <lsjoeberg@protonmail.com>
2024-07-26 15:31:28 +0200

    Improve CLI docs

--- src/cli.rs
+++ src/cli.rs
@@ -6,16 +6,26 @@ use macaddr::MacAddr6;
 const DEFAULT_ADDR: Ipv4Addr = Ipv4Addr::BROADCAST;
 const DEFAULT_PORT: u16 = 9;
 
+/// Send wake-on-lan packets.
+///
+/// Limitations: may not work outside the local network; requires hardware
+/// support in destination computer; most 802.11 wireless interfaces do not
+/// maintain a link in low-power states and cannot receive a magic packet.
 #[derive(Parser, Debug)]
 pub struct Args {
     /// Target NIC 48-bit MAC address
     #[arg()]
     pub mac: MacAddr6,
+
     /// Target IP address
-    #[arg(short = 'i', default_value_t = DEFAULT_ADDR)]
+    ///
+    /// Hint: For a NIC on a local subnet 192.168.10.0/24, use the subnet's
+    /// broadcast address: 192.168.10.255.
+    #[arg(short = 'i', long, default_value_t = DEFAULT_ADDR)]
     pub ip: Ipv4Addr,
-    /// Target port; usually 0, 7, or 9
-    #[arg(short = 'p', default_value_t = DEFAULT_PORT)]
+
+    /// Target port; usually 0, 7 (Echo), or 9 (Discard)
+    #[arg(short = 'p', long, default_value_t = DEFAULT_PORT)]
     pub port: u16,
 }
 
