35d8158e99314889990c7dff0c6d40e0a6c54bfc
Lars Sjöberg <lsjoeberg@protonmail.com>
2024-07-28 14:28:00 +0200

    Add unit tests for packet construction

--- src/lib.rs
+++ src/lib.rs
@@ -29,3 +29,38 @@ pub fn wake_on_lan(ip: Ipv4Addr, port: u16, mac: MacAddr6) -> Result<(), io::Err
     socket.send(&packet)?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use macaddr::MacAddr6;
+
+    use crate::{build_magic_packet, MAGIC_PAYLOAD};
+
+    // A WoL packet is 6 bytes of 0xff, followed by 16 repetitions of the
+    // 48-bit (6 byte) MAC address. This is 6 + 6 * 16 = 102 bytes.
+    const EXPECTED_LEN: usize = 102;
+
+    #[test]
+    fn build_packet_bc() {
+        let mac = MacAddr6::broadcast();
+        let expected = [0xffu8; EXPECTED_LEN];
+
+        let packet = build_magic_packet(mac);
+
+        assert_eq!(packet.len(), EXPECTED_LEN);
+        assert_eq!(packet.as_slice(), expected.as_slice());
+    }
+
+    #[test]
+    fn build_packet_real() {
+        let mac = MacAddr6::new(0, 1, 2, 3, 4, 5);
+        let mut expected = Vec::with_capacity(EXPECTED_LEN);
+        expected.extend_from_slice(&MAGIC_PAYLOAD);
+        expected.extend_from_slice(mac.as_bytes().repeat(16).as_slice());
+
+        let packet = build_magic_packet(mac);
+
+        assert_eq!(packet.len(), EXPECTED_LEN);
+        assert_eq!(packet.as_slice(), expected.as_slice());
+    }
+}
