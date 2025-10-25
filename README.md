fdf507641d3d045d802705feb11526c050720882
Lars Sjöberg <lsjoeberg@protonmail.com>
2025-10-25 17:59:38 +0200

    Implement lib as `no_std`
    * Implement lib with smoltcp
    * Adapt bin 'wolrus' to new lib interface
    * Update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # wolrus
 
-A tiny Rust library for [Wake on LAN].
+A tiny Rust `no_std` library for [Wake on LAN].
 
 ## Usage
 
@@ -14,17 +14,15 @@ cargo add --git https://github.com/lsjoeberg/wolrus
 ```
 
 ```rust
-use std::net::Ipv4Addr;
-use macaddr::MacAddr6;
 use wolrus::wake_on_lan;
 
-fn main() -> Result<(), std::io::Error> {
+fn main() -> Result<(), wolrus::Error> {
     // Broadcast WoL on the local network.
-    let mac = MacAddr6::from([0, 1, 2, 3, 4, 5]);
-    wake_on_lan(mac, None, None)?;
+    let mac_addr = [0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff];
+    wake_on_lan(mac_addr, None, None)?;
 
     // Broadcast WoL on the local subnet.
-    let ip = Ipv4Addr::new(192, 168, 0, 255);
+    let ip = [192, 168, 0, 255];
     wake_on_lan(mac, Some(ip), None)?;
 
     Ok(())
--- src/bin/wolrus.rs
+++ src/bin/wolrus.rs
@@ -1,6 +1,5 @@
-use std::net::Ipv4Addr;
-
 use clap::Parser;
+use core::net::Ipv4Addr;
 use macaddr::MacAddr6;
 use wolrus::wake_on_lan;
 
@@ -19,17 +18,21 @@ pub struct Args {
     ///
     /// Hint: For a NIC on a local subnet 192.168.10.0/24, use the subnet's
     /// broadcast address: 192.168.10.255.
-    #[arg(short = 'i', long, default_value_t = wolrus::DEFAULT_ADDR)]
+    #[arg(short = 'i', long, default_value_t = Ipv4Addr::BROADCAST)]
     pub ip: Ipv4Addr,
 
     /// Target port; usually 0, 7 (Echo), or 9 (Discard)
-    #[arg(short = 'p', long, default_value_t = wolrus::DEFAULT_PORT)]
+    #[arg(short = 'p', long, default_value_t = 9)]
     pub port: u16,
 }
 
 fn main() {
     let args = Args::parse();
-    if let Err(err) = wake_on_lan(args.mac, Some(args.ip), Some(args.port)) {
-        eprintln!("{err}");
+
+    let ip = args.ip.octets();
+    let mac = args.mac.into_array();
+
+    if let Err(err) = wake_on_lan(mac, Some(ip), Some(args.port)) {
+        eprintln!("{err:?}");
     }
 }
